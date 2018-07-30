dd2ce6a4d2a3538ca6f1f7ce2d69c2b9f52d203c
yc yap <yowchun93@gmail.com>
2018-07-31 06:23:36 +0800

    RainDrops Iteration 2
    Removed duplicated check for if the number is not divisible by either 3 or 5 or 7.
    By checking whther the output is empty , we know if the number is divisible or not.

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -2,28 +2,19 @@ package raindrops
 
 import "strconv"
 
-// Convert returns
 func Convert(number int) string {
 	output := ""
-	if notDivisible(number) {
+	if number%3 == 0 {
+		output += "Pling"
+	}
+	if number%5 == 0 {
+		output += "Plang"
+	}
+	if number%7 == 0 {
+		output += "Plong"
+	}
+	if output == "" {
 		output += strconv.Itoa(number)
-	} else {
-		if number%3 == 0 {
-			output += "Pling"
-		}
-		if number%5 == 0 {
-			output += "Plang"
-		}
-		if number%7 == 0 {
-			output += "Plong"
-		}
 	}
 	return output
-}
-
-func notDivisible(number int) bool {
-	if number%3 != 0 && number%5 != 0 && number%7 != 0 {
-		return true
-	}
-	return false
 }
