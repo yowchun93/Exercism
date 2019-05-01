d565e9f4e6d7367ba08a2aa969ff71fe35978321
yc yap <yowchun93@gmail.com>
2019-05-01 15:37:37 +0800

    commit

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -9,15 +9,16 @@ defmodule RNATranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    Enum.map(dna, &rna_to_dna(&1))
+    Enum.map(dna, &match_rna(&1))
   end
 
-  defp rna_to_dna(dna) do
-    case dna do
-      ?G -> ?C
-      ?C -> ?G
-      ?T -> ?A
-      ?A -> ?U
+  defp match_rna(char) do
+    case char do
+      'G' -> 'C'
+      'C' -> 'G'
+      'T' -> 'A'
+      'A' -> 'U'
     end
   end
+
 end
