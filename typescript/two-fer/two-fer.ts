9a3ea9218bac403ce82f092f294f411b6fcd2f48
yowchun <yowchun@coingecko.com>
2021-02-20 15:05:13 +0800

    solution for two-fer

---
+++ typescript/two-fer/two-fer.ts
@@ -0,0 +1,10 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export function twoFer(name?: string): string {
+  if (name === undefined) {
+    return 'One for you, one for me.'
+  }
+  return `One for ${name}, one for me.`
+}
