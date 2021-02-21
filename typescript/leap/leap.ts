8c65279013ec0e3da956989c9fcd7ee0e7b2365c
yowchun <yowchun@coingecko.com>
2021-02-21 09:02:24 +0800

    solution for leap

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,12 +1,16 @@
-function isLeapYear( year: number ) {
-  if (year % 400 == 0) {
-    return true
-  } else if (year % 100) {
-    return false
-  } else if (year % 4) {
-    return true
-  }
-  return false
+// function isLeapYear( year: number ) {
+//   if (year % 400 === 0) {
+//     return true
+//   } else if (year % 100 !== 0) {
+//     return false
+//   } else if (year % 4 === 0) {
+//     return true
+//   }
+//   return false
+// }
+
+function isLeapYear(year: number) {
+  return (year % 4 == 0) && (year % 400 === 0 || year % 100 !== 0);
 }
 
 export default isLeapYear
